cf99d0d05cdb6992e41668d8b0c3a996b39f4464
Mayank Aggarwal <31075014+mayank953@users.noreply.github.com>
2025-07-26 08:49:21 +0530

    streamlit

--- Agentic 2.0/Autogen/7. Autogen Projects/2. Analyser GPT - Modular/main.py
+++ Agentic 2.0/Autogen/7. Autogen Projects/2. Analyser GPT - Modular/main.py
@@ -4,7 +4,6 @@ from models.openai_model_client import get_model_client
 from config.docker_util import getDockerCommandLineExecutor,start_docker_container,stop_docker_container
 from autogen_agentchat.messages import TextMessage
 
-
 async def main():
 
     openai_model_client = get_model_client()
---
+++ Agentic 2.0/Autogen/7. Autogen Projects/2. Analyser GPT - Modular/streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import asyncio
+import os
+
+
+st.title('Analyser GPT- Digital Data Analyzer')
